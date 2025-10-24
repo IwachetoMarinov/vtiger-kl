0d7e018e20e4b7c4a7b96e205bd8acf2423d093b
iwacheto <ivailo@webrika.bg>
2025-10-24 13:56:37 +0300

    Xreate cron jobs for Sync Metals and Orders

--- cron/vtiger_order_cron.php
+++ cron/vtiger_order_cron.php
@@ -1,15 +1,15 @@
 <?php
 
 require_once 'includes/main/WebUI.php';
-include_once 'modules/Metals/MetalsCron.php';
+include_once 'modules/Metals/OrderCron.php';
 
 try {
-    echo "Cron execution in MetalsCron started at " . date('Y-m-d H:i:s') . PHP_EOL;
+    echo "Cron execution in OrderCron started at " . date('Y-m-d H:i:s') . PHP_EOL;
 
     // TODO logic to hit ERP and fetch metal prices
-    
-    $job = new MetalsCron();
-    $job->createDailyMetal();
+
+    $job = new OrdersCron();
+    $job->createDailyOrders();
     echo "\n Cron execution ended at " . date('Y-m-d H:i:s') . PHP_EOL;
 } catch (Exception $e) {
     echo 'ERROR: ' . $e->getMessage() . PHP_EOL;
--- test-cron.php
+++ test-cron.php
@@ -1,17 +1,19 @@
 <?php
 
-
 require_once 'includes/main/WebUI.php';
-
-
+include_once 'modules/Metals/OrderCron.php';
+// include_once 'modules/Metals/MetalsCron.php';
 
 try {
-    echo "Cron execution in EmailsCron started at " . date('Y-m-d H:i:s') . PHP_EOL;
+    echo "Cron execution in MetalsCron started at " . date('Y-m-d H:i:s') . PHP_EOL;
+    // TODO logic to hit ERP and fetch metal prices
+    // $job = new MetalsCron();
+    // $job->createDailyMetal();
+
+    // Todo logic to create orders
+    $job = new OrdersCron();
+    $job->createDailyOrders();
+    echo "\n Cron execution ended at " . date('Y-m-d H:i:s') . PHP_EOL;
 } catch (Exception $e) {
     echo 'ERROR: ' . $e->getMessage() . PHP_EOL;
 }
-
-// Empty row
-echo "\n";
-
-echo "Cron execution ended at " . date('Y-m-d H:i:s') . PHP_EOL;
