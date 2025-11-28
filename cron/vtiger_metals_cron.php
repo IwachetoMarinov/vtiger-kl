50ac59e32790d138124ab259fcaa0b2e3637170d
iwacheto <ivailo@webrika.bg>
2025-11-28 16:42:23 +0200

    Disable Metals cron

--- cron/vtiger_metals_cron.php
+++ cron/vtiger_metals_cron.php
@@ -6,9 +6,10 @@ include_once 'modules/Assets/MetalsCron.php';
 try {
     echo "Cron execution in MetalsCron started at " . date('Y-m-d H:i:s') . PHP_EOL;
 
-    $job = new MetalsCron();
+    // DISABLE for now
+    // $job = new MetalsCron();
     // Run the daily metal update operation
-    $job->createUpdateDailyMetal();
+    // $job->createUpdateDailyMetal();
 
     echo "\n Cron execution ended at " . date('Y-m-d H:i:s') . PHP_EOL;
 } catch (Exception $e) {
