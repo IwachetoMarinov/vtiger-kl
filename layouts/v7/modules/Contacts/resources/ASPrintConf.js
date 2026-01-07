7d63a7694e69a37ce84ae456d99b64dc74f89c1a
iwacheto <ivailo@webrika.bg>
2026-01-07 17:49:10 +0200

    Remove unusual logs from ASPrintconffg

--- layouts/v7/modules/Contacts/resources/ASPrintConf.js
+++ layouts/v7/modules/Contacts/resources/ASPrintConf.js
@@ -1,27 +1,9 @@
 jQuery(function () {
-  console.log("[PrintConf] JS loaded");
-
   function initDatePickers() {
-    console.log("[PrintConf] initDatePickers() called");
+    if (typeof jQuery.fn.datepicker === "undefined") return;
 
-    console.log("[PrintConf] datepicker type:", typeof jQuery.fn.datepicker);
-    console.log("[PrintConf] start exists:", jQuery("#start_date").length);
-    console.log("[PrintConf] end exists:", jQuery("#end_date").length);
+    if (!jQuery("#start_date").length || !jQuery("#end_date").length) return;
 
-    if (typeof jQuery.fn.datepicker === "undefined") {
-      console.error("[PrintConf] datepicker not loaded");
-      return;
-    }
-
-    if (!jQuery("#start_date").length || !jQuery("#end_date").length) {
-      console.error("[PrintConf] Inputs not found in DOM");
-      return;
-    }
-
-    const format = jQuery("#start_date").data("date-format") || "mm-dd-yyyy";
-    console.log("[PrintConf] format:", format);
-
-    // Hard reset
     try {
       jQuery("#start_date").datepicker("destroy");
     } catch (e) {}
@@ -29,7 +11,6 @@ jQuery(function () {
       jQuery("#end_date").datepicker("destroy");
     } catch (e) {}
 
-    // IMPORTANT: Use body container (most reliable in fixed modals)
     jQuery("#start_date")
       .datepicker({
         format: "yyyy-mm-dd",
@@ -51,11 +32,6 @@ jQuery(function () {
       .on("changeDate", function () {
         jQuery(this).datepicker("hide");
       });
-
-    console.log(
-      "[PrintConf] datepicker inited. start data:",
-      jQuery("#start_date").data("datepicker")
-    );
 
     // Force z-index always
     jQuery(document)
@@ -68,7 +44,6 @@ jQuery(function () {
     jQuery("#start_date")
       .off(".printconfOpen")
       .on("click.printconfOpen focus.printconfOpen", function () {
-        console.log("[PrintConf] open start");
         jQuery(this).datepicker("show");
         jQuery(".datepicker").css("z-index", 200000);
       });
@@ -76,7 +51,6 @@ jQuery(function () {
     jQuery("#end_date")
       .off(".printconfOpen")
       .on("click.printconfOpen focus.printconfOpen", function () {
-        console.log("[PrintConf] open end");
         jQuery(this).datepicker("show");
         jQuery(".datepicker").css("z-index", 200000);
       });
@@ -85,7 +59,6 @@ jQuery(function () {
     jQuery(".js-open-start")
       .off(".printconfOpen")
       .on("click.printconfOpen", function () {
-        console.log("[PrintConf] icon open start");
         jQuery("#start_date").datepicker("show");
         jQuery(".datepicker").css("z-index", 200000);
       });
@@ -93,7 +66,6 @@ jQuery(function () {
     jQuery(".js-open-end")
       .off(".printconfOpen")
       .on("click.printconfOpen", function () {
-        console.log("[PrintConf] icon open end");
         jQuery("#end_date").datepicker("show");
         jQuery(".datepicker").css("z-index", 200000);
       });
@@ -102,41 +74,30 @@ jQuery(function () {
     jQuery("#start_date")
       .off(".printconfRange")
       .on("changeDate.printconfRange", function (e) {
-        console.log("[PrintConf] start changed", e.date);
         jQuery("#end_date").datepicker("setStartDate", e.date);
       });
 
     jQuery("#end_date")
       .off(".printconfRange")
       .on("changeDate.printconfRange", function (e) {
-        console.log("[PrintConf] end changed", e.date);
         jQuery("#start_date").datepicker("setEndDate", e.date);
       });
   }
 
   function openModal() {
-    console.log("[PrintConf] openModal()");
     jQuery("#myModal").show();
-
-    // Make sure modal is actually visible
-    console.log(
-      "[PrintConf] modal visible:",
-      jQuery("#myModal").is(":visible")
-    );
 
     // init after visible
     setTimeout(initDatePickers, 50);
   }
 
   function closeModal() {
-    console.log("[PrintConf] closeModal()");
     jQuery("#myModal").hide();
   }
 
   // OPEN
   jQuery("body").on("click", "#printConf", function (e) {
     e.preventDefault();
-    console.log("[PrintConf] #printConf clicked");
     openModal();
   });
 
@@ -154,13 +115,10 @@ jQuery(function () {
   });
 
   jQuery("#start_date").on("changeDate", function () {
-    console.log("Close Datepicker on start date");
-
     jQuery(this).datepicker("hide");
   });
 
   jQuery("#end_date").on("changeDate", function () {
-    console.log("Close Datepicker on end date");
     jQuery(this).datepicker("hide");
   });
 
