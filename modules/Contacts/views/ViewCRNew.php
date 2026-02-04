594d43b48ce76a90809e644d6e4b5789a63824a9
iwacheto <ivailo@webrika.bg>
2026-02-04 18:03:14 +0200

    Try to adjust Template and editable forms

--- modules/Contacts/views/ViewCRNew.php
+++ modules/Contacts/views/ViewCRNew.php
@@ -163,8 +163,8 @@ class Contacts_ViewCRNew_View extends Vtiger_Index_View
         $fieldH = 5.0;
 
         // TABLE: set these using debug grid so row 1 sits exactly in the first blank row
-        $startY  = 107.0;   // <-- adjust
-        $rowStep = 7.6;     // <-- adjust
+        $startY  = 108.0;   // <-- adjust
+        $rowStep = 7.45;     // <-- adjust
         // Columns: adjust these to match your table
         $xQty    = 20.5;
         $wQty    = 16.5;
