eb5a3b2f03345707a1eb1833455f4be7f1054c0b
iwacheto <ivailo@webrika.bg>
2026-02-12 09:58:06 +0200

    Adjust sizes of Metals table inputs in PO page

--- modules/Contacts/views/PurchaseOrderView.php
+++ modules/Contacts/views/PurchaseOrderView.php
@@ -386,7 +386,7 @@ class Contacts_PurchaseOrderView_View extends Vtiger_Index_View
         $startX = 57.3;   // was ~48.0
         $startY = 111.0;  // was ~116.0
         $cellW  = 13.57;   // was ~15
-        $cellH  = 6.45;    // was ~7
+        $cellH  = 6.35;    // was ~7
 
         $metalCount  = 4;   // Gold, Silver, Platinum, Palladium
         $weightCount = 9;   // 1000oz ... Other
