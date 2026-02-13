310f6f127e948324bee3aec0766f90b40a6daa41
iwacheto <ivailo@webrika.bg>
2026-02-13 14:50:00 +0200

    Get currency from Intent

--- modules/GPMIntent/views/Detail.php
+++ modules/GPMIntent/views/Detail.php
@@ -8,7 +8,8 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  * *********************************************************************************** */
-
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 class GPMIntent_Detail_View extends Vtiger_Detail_View
 {
 	public function getDetailViewLinks($linkParams)
@@ -46,11 +47,26 @@ class GPMIntent_Detail_View extends Vtiger_Detail_View
 		$products = GPMIntent_Line_Model::getInstanceByIntent($recordId);
 
 		$recordModel = Vtiger_Record_Model::getInstanceById($recordId);
+		$moduleName  = $recordModel->getModuleName();
 
-		$currency = $recordModel->get('cf_1132');
+		// Get module model properly
+		$moduleModel = Vtiger_Module_Model::getInstance($moduleName);
+
+		$currencyValue = null;
+
+		// THIS is correct place to call getFields()
+		foreach ($moduleModel->getFields() as $fieldModel) {
+
+			if (strcasecmp(trim($fieldModel->get('label')), 'Currency') === 0) {
+
+				$realFieldName = $fieldModel->get('name');
+				$currencyValue = $recordModel->get($realFieldName);
+				break;
+			}
+		}
 
 		$viewer = $this->getViewer($request);
-		$viewer->assign('CURRENCY', $currency);
+		$viewer->assign('CURRENCY', $currencyValue);
 		$viewer->assign('RELATED_PRODUCTS', $products);
 
 		return parent::showModuleBasicView($request);
