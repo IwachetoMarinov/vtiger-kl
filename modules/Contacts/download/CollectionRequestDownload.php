31fab673f8cec5abfdf9b0bbfad741ee97151cd3
iwacheto <ivailo@webrika.bg>
2026-03-05 13:53:03 +0200

    Fix PrintConf js file

--- modules/Contacts/download/CollectionRequestDownload.php
+++ modules/Contacts/download/CollectionRequestDownload.php
@@ -65,7 +65,6 @@ class CollectionRequestDownload
         [$htmlPath, $basePdfPath, $finalPdfPath] = CoreDownload::buildPaths($tmpDir, $fileName);
 
         // HTML -> base PDF
-        $html = '<base href="file://' . rtrim($root_directory, "/\\") . '/">' . $html;
         CoreDownload::writeFileOrFail($htmlPath, (string)$html);
         CoreDownload::runWkhtmltopdfOrFail($htmlPath, $basePdfPath, self::WKHTML_OPTS);
         @unlink($htmlPath);
