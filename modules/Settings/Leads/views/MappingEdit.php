afa9d5b355848da9500a747ad42c485387493023
iwacheto <ivailo@webrika.bg>
2026-03-24 16:33:32 +0200

    Add debug for Leads Maping module

--- modules/Settings/Leads/views/MappingEdit.php
+++ modules/Settings/Leads/views/MappingEdit.php
@@ -8,39 +8,67 @@
  * All Rights Reserved.
  ************************************************************************************/
 
-class Settings_Leads_MappingEdit_View extends Settings_Vtiger_Index_View {
+class Settings_Leads_MappingEdit_View extends Settings_Vtiger_Index_View
+{
 
-	public function requiresPermission(\Vtiger_Request $request) {
+	public function requiresPermission(\Vtiger_Request $request)
+	{
 		$permissions = parent::requiresPermission($request);
 		$permissions[] = array('module_parameter' => 'module', 'action' => 'DetailView');
 		return $permissions;
 	}
-    
-	function checkPermission(Vtiger_Request $request) {
+
+	function checkPermission(Vtiger_Request $request)
+	{
 		return parent::checkPermission($request);
 	}
-    
-    public function process(Vtiger_Request $request) {
-		$qualifiedModuleName = $request->getModule(false);
-		$viewer = $this->getViewer($request);
-		
-		$viewer->assign('MODULE_MODEL', Settings_Leads_Mapping_Model::getInstance());
-		$viewer->assign('LEADS_MODULE_MODEL', Settings_Leads_Module_Model::getInstance('Leads'));
-		$viewer->assign('ACCOUNTS_MODULE_MODEL', Settings_Leads_Module_Model::getInstance('Accounts'));
-		$viewer->assign('CONTACTS_MODULE_MODEL', Settings_Leads_Module_Model::getInstance('Contacts'));
-		$viewer->assign('POTENTIALS_MODULE_MODEL', Settings_Leads_Module_Model::getInstance('Potentials'));
 
-		$viewer->assign('QUALIFIED_MODULE', $qualifiedModuleName);
-		$viewer->assign('RESTRICTED_FIELD_IDS_LIST', Settings_Leads_Mapping_Model::getRestrictedFieldIdsList());
-		$viewer->view('LeadMappingEdit.tpl', $qualifiedModuleName);
+	public function process(Vtiger_Request $request)
+	{
+		echo "START<br>";
+		flush();
+		echo "1<br>";
+		flush();
+		$qualifiedModuleName = $request->getModule(false);
+
+		echo "2<br>";
+		flush();
+		$viewer = $this->getViewer($request);
+
+		echo "3<br>";
+		flush();
+		var_dump(Settings_Leads_Mapping_Model::getInstance());
+
+		echo "4<br>";
+		flush();
+		var_dump(Settings_Leads_Module_Model::getInstance('Leads'));
+
+		echo "5<br>";
+		flush();
+		var_dump(Settings_Leads_Module_Model::getInstance('Accounts'));
+
+		echo "6<br>";
+		flush();
+		var_dump(Settings_Leads_Module_Model::getInstance('Contacts'));
+
+		echo "7<br>";
+		flush();
+		var_dump(Settings_Leads_Module_Model::getInstance('Potentials'));
+
+		echo "8<br>";
+		flush();
+		var_dump(Settings_Leads_Mapping_Model::getRestrictedFieldIdsList());
+
+		die("STOP HERE");
 	}
-	
+
 	/**
 	 * Function to get the list of Script models to be included
 	 * @param Vtiger_Request $request
 	 * @return <Array> - List of Vtiger_JsScript_Model instances
 	 */
-	function getHeaderScripts(Vtiger_Request $request) {
+	function getHeaderScripts(Vtiger_Request $request)
+	{
 		$headerScriptInstances = parent::getHeaderScripts($request);
 		$moduleName = $request->getModule();
 
