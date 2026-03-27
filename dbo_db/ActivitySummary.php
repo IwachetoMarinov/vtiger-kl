d90dc79aa10553ab3b9bb3cede7e561438624300
iwacheto <ivailo@webrika.bg>
2026-03-27 13:35:39 +0200

    Remove dumps for data from ERP

--- dbo_db/ActivitySummary.php
+++ dbo_db/ActivitySummary.php
@@ -299,10 +299,6 @@ class ActivitySummary
 
             $summary = GetDBRows::getRows($this->connection, $sql, $params);
 
-            echo "<pre>";
-            print_r($summary);
-            echo "</pre>";
-
             $items = $this->mapTransactionItems($summary, $transaction);
 
             $transaction['barItems'] = $items;
