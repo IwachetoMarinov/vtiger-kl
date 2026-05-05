35465a9bdbd1d6248271851817aeb007b9bd269a
iwacheto <ivailo@webrika.bg>
2026-05-05 10:13:50 +0300

    Add more logs for test SH cron job

--- modules/Contacts/cron/MonthlyStatementOfHoldings.php
+++ modules/Contacts/cron/MonthlyStatementOfHoldings.php
@@ -17,7 +17,7 @@ class Contacts_MonthlyStatementOfHoldings
         echo "Starting Monthly Statement of Holdings Cron...\n";
 
         // 1. Check if not first day of the month, if yes then exit (to avoid running on the first day of the month)
-        if (date('d') !== '01') return;
+        // if (date('d') !== '01') return;
 
         // 2. Build date range for the current month
         $date_range = Contacts_CronHelpers::buildMonthlyDateRange();
